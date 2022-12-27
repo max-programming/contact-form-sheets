6e33d1dc2f34772ca34ce8ba699d07f7ef9c41fc
Max Programming <ui.sabuwala@gmail.com>
2022-12-27 19:36:18 +0530

    Update files according to video

--- src/index.js
+++ src/index.js
@@ -15,26 +15,29 @@ app.use(express.static('public'));
 
 app.post('/send-message', async (req, res) => {
   try {
-    const data = contactFormSchema.parse(req.body);
-    const keys = Object.keys(data);
-    const rows = keys.map(key => data[key]);
+    const body = contactFormSchema.parse(req.body);
+
+    // Object to Sheets
+    const rows = Object.values(body);
+    console.log(rows);
+
     await sheets.spreadsheets.values.append({
       spreadsheetId: SHEET_ID,
-      range: 'Sheet1!A2:C2',
+      range: 'Data!A:C',
       insertDataOption: 'INSERT_ROWS',
       valueInputOption: 'RAW',
       requestBody: {
         values: [rows],
       },
     });
-    res.json({ keys, rows });
+    res.json({ message: 'Data added successfully' });
   } catch (error) {
     if (error instanceof ZodError) {
       res.status(400).json({ error: error.message });
     } else {
-      res.status(500).json({ error });
+      res.status(400).json({ error });
     }
   }
 });
 
-app.listen(5000, () => console.log('Listening on port 5000'));
+app.listen(5000, () => console.log(`App running on http://localhost:5000`));
--- src/sheetClient.js
+++ src/sheetClient.js
@@ -2,7 +2,7 @@ import { google } from 'googleapis';
 
 import key from '../secrets.json' assert { type: 'json' };
 
-export const SHEET_ID = '1gOarmZImf9tZSLs6wyDVcqI2x4gvJME9KJt-YJpl-qc';
+export const SHEET_ID = '1NF5B-u6RE5oURxW3e9XU8Ts6Hcft0H1pSyiYESu9IdY';
 
 const client = new google.auth.JWT(key.client_email, null, key.private_key, [
   'https://www.googleapis.com/auth/spreadsheets',
